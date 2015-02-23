3f17ad7063c92a06a2754495321281ca00ed2f55
Ryan Reynolds <ryanrey@uga.edu>
2015-02-22 19:14:38 -0500

    Create matches.php

---
+++ matches.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link type='text/css' rel='stylesheet' href='nerdluv.css'/>
+    <title>PHP!</title>
+    <!--Add a header with picture, logo is broken on elc-->
+  </head>
+  <body>
+  	<div>
+	<form> <!-- set the form action to our php submit file-->
+		<fieldset>
+		<label for = "name">Name:</label>
+		<input type = "text" name = "name" value = "" id = "name" size = "16"/>
+		<br>
+		<button style = type="submit"> <b>View My Matches</b></button> <!-- add submit function on onclick-->
+	<!--add a submit button-->
+	</fieldset>
+	</form>
+</div>
+</body>
+</html>
