c7a3e98e288b6f307f855fee746e5a28e51269d2
chongt <tiffue@uga.edu>
2015-02-25 23:18:16 -0500

    Update signup-submit.php

--- signup-submit.php
+++ signup-submit.php
@@ -1,28 +1,6 @@
 <!DOCTYPE html>
 <html>
 
-<!-- writes in values from the form into single.txt file.!-->
-<?php
-$file = 'singles.txt';
-$current = file_get_contents($file);
-$current .= $_POST["nameBox"];
-$current .= ",";
-if($_POST["sex"] == "male")  $current .="m"; 
-else $current .= "f";
-$current .= ",";
-$current .= $_POST["age"];
-$current .= ",";
-$current .= strtoupper($_POST["personType"]);
-$current .= ",";
-$current .= $_POST["compType"];
-$current .= ",";
-$current .= $_POST["min"];
-$current .= ",";
-$current .= $_POST["max"];
-$current .= "\n";
-file_put_contents($file, $current);
-?>
-
 	<head>
 		<title>NerdLuv</title>
 		
@@ -41,6 +19,70 @@ file_put_contents($file, $current);
 			<img src="https://webster.cs.washington.edu/images/nerdluv/nerdluv.png" alt="banner logo" /> <br />
 			where meek geeks meet
 		</div>
+
+<!-- writes in values from the form into single.txt file.!-->
+		<?php
+		$valid = false;
+		$error = "Error!";
+		$file = 'singles.txt';
+
+		$current = file_get_contents($file);
+		if($_POST["nameBox"]!= ""){
+			$current .= $_POST["nameBox"];
+			$valid = true;
+		}
+		else{
+			$valid = false;
+		}
+		$current .= ",";
+		if($_POST["sex"] == "male"){
+			$current .="m"; 
+		}
+		else{
+			$current .= "f";
+		}
+		$current .= ",";
+		if($_POST["age"] > 1){
+			$current .= $_POST["age"];
+			$valid = true;
+		}
+		else{
+			$valid = false;
+		}
+		$current .= ",";
+		if($_POST["personType"]!= ""){
+			$current .= strtoupper($_POST["personType"]);
+			$valid = true;
+		}
+		else{
+			$valid = false;
+		}
+		$current .= ",";
+		if($_POST["compType"]!= ""){
+			$current .= $_POST["compType"];
+		}
+		$current .= ",";
+		if($_POST["min"] > 1){
+			$current .= $_POST["min"];
+			$valid = true;
+		}
+		else{
+			$valid = false;
+		}
+		$current .= ",";
+		if($_POST["max"] > $_POST["min"]){
+			$current .= $_POST["max"];
+			$valid = true;
+		}
+		else{
+			$valid = false;
+		}
+		$current .= ",\n";
+		if($valid == true){
+			file_put_contents($file, $current);
+		}
+
+		?>
 
 		<div>
 			<h1>Thank you!</h1>
