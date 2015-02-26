4180faee5a131672bcf35946a289ee6ff1376744
chongt <tiffue@uga.edu>
2015-02-25 23:46:51 -0500

    Update signup-submit.php

--- signup-submit.php
+++ signup-submit.php
@@ -80,7 +80,7 @@
 
 		<?php if($valid == false){ ?>
 		<div>
-			<h1>Error!</h1>
+			<h1>Error! Invalid data.</h1>
 			<p>We're sorry. You submitted invalid user information. Please go back and try again.</p>
 		</div>
 		<?php
