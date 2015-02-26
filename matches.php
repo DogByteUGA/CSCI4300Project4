40ffb7d87cb28ea3014dc1e3c32f66da8f876a73
D Mays <dmmays@uga.edu>
2015-02-26 00:02:19 -0500

    Update matches.php
    added php include function for top and bottom

--- matches.php
+++ matches.php
@@ -1,51 +1,19 @@
 <html>
 <head>
 	<meta charset="utf-8" />
-		
-		<!-- instructor-provided CSS and JavaScript links; do not modify -->
-		<link href="https://webster.cs.washington.edu/images/nerdluv/heart.gif" type="image/gif" rel="shortcut icon" />
-		<link href="https://webster.cs.washington.edu/css/nerdluv.css" type="text/css" rel="stylesheet" />
-		
-		<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
-		<script src="https://webster.cs.washington.edu/js/nerdluv/provided.js" type="text/javascript"></script>
+<?php 	include 'top.html'; ?>
 	<title>Signup</title>
 </head>
 <body>
-	<div id="bannerarea">
-			<img src="https://webster.cs.washington.edu/images/nerdluv/nerdluv.png" alt="banner logo" /> <br />
-			where meek geeks meet
-		</div>
 	<form action = "matches-submit.php" method ="get">
 		<fieldset>
 			<legend> Returning User:</legend>
-		<strong><label>Name:</label></strong>
-			<input type="text" name="nameBox" maxlength="16">		
-			<br><br>
+		<label>Name:</label>
+			<input type="text" name="nameBox" maxlength="16">
+		<br>
 		<input type="submit" name="infoSub" value="View My Matches">
 		</fieldset>
 	</form>
-	<div>
-			<p>
-				This page is for single nerds to meet and date each other!  Type in your personal information and wait for the nerdly luv to begin!  Thank you for using our site.
-			</p>
-			
-			<p>
-				Results and page (C) Copyright NerdLuv Inc.
-			</p>
-			
-			<ul>
-				<li>
-					<a href="index.php">
-						<img src="https://webster.cs.washington.edu/images/nerdluv/back.gif" alt="icon" />
-						Back to front page
-					</a>
-				</li>
-			</ul>
-		</div>
-
-		<div id="w3c">
-			<a href="https://webster.cs.washington.edu/validate-html.php"><img src="https://webster.cs.washington.edu/images/w3c-html.png" alt="Valid HTML" /></a>
-			<a href="https://webster.cs.washington.edu/validate-css.php"><img src="https://webster.cs.washington.edu/images/w3c-css.png" alt="Valid CSS" /></a>
-		</div>
 </body>
+<?php 	include 'bottom.html'; ?>
 </html>
