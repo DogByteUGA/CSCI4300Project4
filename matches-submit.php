99c17601072a7e4bac92f2670df8f9724959e7b6
mkeeton20 <mikekeeton@Mikes-Mac-Pro.local>
2015-02-25 22:49:10 -0500

    new stuff

--- matches-submit.php
+++ matches-submit.php
@@ -28,21 +28,23 @@
 		$user = $_GET["nameBox"];
 
 
-		while(fgets($singles)!= false){
+		while(!feof($singles)){
 			
-			$line = fgests($singles);
+			$line = fgets($singles);
 			$person = explode(",", $line);
 			if ($person[0]==$user) {
 				$user = $person;
 				break;
 			}
-			if($user==null){
-				die("The user was not found");
-			}
+		
 		}
-
-		while(fgets($singles)!= false){
-			$line = fgests($singles);
+			
+			if($user==null){
+			die("The user was not found");
+			}
+			$singles = fopen("singles.txt", "r") or die("Unable to open file!");
+		while(!feof($singles)){
+			$line = fgets($singles);
 			$person = explode(",", $line);
 			if ($person[1]!=$user[1]) {
 				if($person[2]<$user[6]){
@@ -51,19 +53,7 @@
 							if($user[2]>$person[5]){
 								if($person[4]==$user[4]){
 									if(substr($person[3],0,1)==substr($user[3],0,1) || substr($person[3],1,2)==substr($user[3],1,2) || substr($person[3],2,3)==substr($user[3],2,3) || substr($person[3],3,4)==substr($user[3],3,4) ){
-										echo "<div class="match">
-				<p>
-					<img src="https://webster.cs.washington.edu/images/nerdluv/user.jpg" alt="photo" />"
-					.$person[0].
-				"</p>
-
-				<ul>
-					<li><strong>gender:</strong>".  $person[1]."</li>
-					<li><strong>age:</strong>     ".$person[2]."</li>
-					<li><strong>type:</strong>    ".$person[3]."</li>
-					<li><strong>OS:</strong>      ".$person[4]."</li>
-				</ul>
-			</div>";
+										echo "<div class='match'> <p> <img src='https:webster.cs.washington.edu/images/nerdluv/user.jpg' alt='photo' />" .$person[0]."</p><ul><li><strong>gender:</strong>".  $person[1]."</li><li><strong>age:</strong>     ".$person[2]."</li><li><strong>type:</strong>    ".$person[3]."</li><li><strong>OS:</strong>      ".$person[4]."</li></ul></div>";
 									}
 								}
 							}
